5b4da6a2f478ff5bd6bd449666f84088403aa219
luyosamson <luyosamson@gmail.com>
2022-08-22 07:16:41 +0300

    Assignment completed

---
+++ src/00-objectball.js
@@ -0,0 +1,103 @@
+function gameObject() {
+    let obj = {
+      away: {team: 'Charlotte Hornets',
+             colors: ['Turquoise', 'Purple'],
+             players: {
+             'Jeff Adrien':{ number: 4,
+                            shoe: 18,
+                            points: 10,
+                            rebounds: 1,
+                            assists: 1,
+                            steals: 2,
+                            blocks: 7,
+                            slamDunks: 2},
+            'Bismak Biyombo':{ number: 0,
+                            shoe: 16,
+                            points: 12,
+                            rebounds: 4,
+                            assists: 7,
+                            steals: 7,
+                            blocks: 15,
+                            slamDunks: 10},
+            'Desagna Diop':{number: 2,
+                            shoe: 14,
+                            points: 24,
+                            rebounds: 12,
+                            assists: 12,
+                            steals: 4,
+                            blocks: 5,
+                            slamDunks: 5},
+            'Ben Gordon':{number: 8,
+                            shoe: 15,
+                            points: 33,
+                            rebounds: 3,
+                            assists: 2,
+                            steals: 1,
+                            blocks: 1,
+                            slamDunks: 0},
+            'Brendan Haywood':{number: 33,
+                            shoe: 15,
+                            points: 6,
+                            rebounds: 12,
+                            assists: 12,
+                            steals: 22,
+                            blocks: 5,
+                            slamDunks: 12}}},
+
+      home:{team: 'Brooklyn Nets',
+      colors: ['black', 'white'],
+      players: {
+                'Alan Anderson': {number: 0,
+                                shoe: 16,
+                                points: 22,
+                                rebounds: 12,
+                                assists: 12,
+                                steals: 3,
+                                blocks: 1,
+                                slamDunks: 1},
+                'Reggie Evans':{number: 30,
+                                shoe: 14,
+                                points: 22,
+                                rebounds: 12,
+                                assists: 12,
+                                steals: 12,
+                                blocks: 12,
+                                slamDunks: 7},
+                'Brook Lopez': { number: 11,
+                                shoe: 17,
+                                points: 17,
+                                rebounds: 19,
+                                assists: 10,
+                                steals: 3,
+                                blocks: 1,
+                                slamDunks: 5},
+                'Mason Plumlee':{
+                                number: 1,
+                                shoe: 19,
+                                points: 26,
+                                rebounds: 12,
+                                assists: 6,
+                                steals: 3,
+                                blocks: 8,
+                                slamDunks: 5,},
+                'Jason Terry':{number: 31,
+                                shoe: 15,
+                                points: 19,
+                                rebounds: 2,
+                                assists: 2,
+                                steals: 4,
+                                blocks: 11,
+                                slamDunks: 1}}},
+    }
+    return obj
+  } gameObject()
+
+  function numPointsScored(name){
+      let obj = gameObject()
+      
+      if(obj.away.players[name] !== undefined){
+        return (obj.away.players[name].points)
+        }else if(obj.home.players[name] !== undefined){
+            return (obj.home.players[name].points)
+        }else {return 'players in not home, and not away'}
+  } console.log(numPointsScored('alex'))
